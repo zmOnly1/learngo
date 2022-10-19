2ce9c8e4e83dc10c8373c5fba525c0a9a78938b5
zmOnly1 <wenzhimin20>
2022-10-19 22:37:01 +0800

    compose

--- tree/entry/entry.go
+++ tree/entry/entry.go
@@ -5,6 +5,21 @@ import (
 	"learngo2/tree"
 )
 
+type myTreeNode struct {
+	node *tree.Node
+}
+
+func (myNode *myTreeNode) postOrder() {
+	if myNode == nil || myNode.node == nil {
+		return
+	}
+	leftNode := myTreeNode{myNode.node.Left}
+	leftNode.postOrder()
+	rightNode := myTreeNode{myNode.node.Right}
+	rightNode.postOrder()
+	myNode.node.Print()
+}
+
 func main() {
 	var root tree.Node
 	root = tree.Node{Value: 3}
@@ -13,31 +28,35 @@ func main() {
 	root.Right.Left = new(tree.Node)
 	root.Left.Right = tree.CreateNode(2)
 
-	nodes := []tree.Node{
-		{Value: 3},
-		{},
-		{6, nil, &root},
-	}
-	fmt.Println(nodes)
-
-	root.Print()
-	root.Right.Left.SetValue(4)
-	root.Right.Left.Print()
-
-	root.Print()
-	root.SetValue(100)
-
-	pRoot := &root
-	pRoot.Print()
-	pRoot.SetValue(200)
-	pRoot.Print()
-
-	var bRoot *tree.Node //bRoot == nil
-	bRoot.SetValue(200)
-	bRoot = &root
-	bRoot.SetValue(300)
-	bRoot.Print()
+	//nodes := []tree.Node{
+	//	{Value: 3},
+	//	{},
+	//	{6, nil, &root},
+	//}
+	//fmt.Println(nodes)
+	//
+	//root.Print()
+	//root.Right.Left.SetValue(4)
+	//root.Right.Left.Print()
+	//
+	//root.Print()
+	//root.SetValue(100)
+	//
+	//pRoot := &root
+	//pRoot.Print()
+	//pRoot.SetValue(200)
+	//pRoot.Print()
+	//
+	//var bRoot *tree.Node //bRoot == nil
+	//bRoot.SetValue(200)
+	//bRoot = &root
+	//bRoot.SetValue(300)
+	//bRoot.Print()
 
 	fmt.Println("start traverse...")
 	root.Traverse()
+
+	fmt.Println("start post traverse...")
+	node := myTreeNode{&root}
+	node.postOrder()
 }
