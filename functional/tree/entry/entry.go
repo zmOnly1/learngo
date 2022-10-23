22bf892ea89203f12cb50d56ef75485d6935c953
zmOnly1 <wenzhimin20>
2022-10-23 15:00:40 +0800

    functional advance

---
+++ functional/tree/entry/entry.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"learngo2/functional/tree"
+)
+
+func main() {
+	var root tree.Node
+	root = tree.Node{Value: 3}
+	root.Left = &tree.Node{}
+	root.Right = &tree.Node{Value: 5}
+	root.Right.Left = new(tree.Node)
+	root.Left.Right = tree.CreateNode(2)
+	root.Right.Left.SetValue(4)
+
+	root.Traverse()
+
+	nodeCount := 0
+	root.TraverseFunc(func(node *tree.Node) {
+		nodeCount++
+	})
+	fmt.Println("Node count:", nodeCount)
+}
---
+++ functional/tree/traversal.go
@@ -0,0 +1,39 @@
+package tree
+
+import "fmt"
+
+type Node struct {
+	Value       int
+	Left, Right *Node
+}
+
+func (node *Node) Print() {
+	fmt.Println(node.Value)
+}
+
+func (node *Node) SetValue(value int) {
+	if node == nil {
+		fmt.Println("Setting nil Value ignored")
+		return
+	}
+	node.Value = value
+}
+
+func CreateNode(value int) *Node {
+	return &Node{Value: value}
+}
+
+func (node *Node) Traverse() {
+	node.TraverseFunc(func(n *Node) {
+		n.Print()
+	})
+}
+
+func (node *Node) TraverseFunc(f func(*Node)) {
+	if node == nil {
+		return
+	}
+	node.Left.TraverseFunc(f)
+	f(node)
+	node.Right.TraverseFunc(f)
+}
