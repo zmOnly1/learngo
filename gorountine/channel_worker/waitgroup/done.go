b58b17e7e030a0fe4f49f4480db5d3fe23bb0260
zmOnly1 <wenzhimin20>
2022-10-23 22:15:43 +0800

    wait group

---
+++ gorountine/channel_worker/waitgroup/done.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func doWorker(id int, c chan int, wg *sync.WaitGroup) {
+	for n := range c {
+		fmt.Printf("worker %d received %c\n", id, n)
+		wg.Done()
+	}
+}
+
+func createWorker(id int, wg *sync.WaitGroup) worker {
+	w := worker{
+		in: make(chan int),
+		wg: wg,
+	}
+	go doWorker(id, w.in, wg)
+	return w
+}
+
+type worker struct {
+	in chan int
+	wg *sync.WaitGroup
+}
+
+func chanDemo() {
+	var wg sync.WaitGroup
+	wg.Add(20)
+
+	var workers [10]worker
+	for i := 0; i < 10; i++ {
+		workers[i] = createWorker(i, &wg)
+	}
+
+	for i, worker := range workers {
+		worker.in <- 'a' + i
+	}
+
+	for i, worker := range workers {
+		worker.in <- 'A' + i
+	}
+
+	wg.Wait()
+}
+
+func main() {
+	chanDemo()
+}
