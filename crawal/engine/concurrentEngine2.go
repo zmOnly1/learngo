b9906b7eecb2af848ab989d9d7e4fd03d6d9425d
zmOnly1 <wenzhimin20>
2022-10-28 09:59:49 +0800

    concurrent

---
+++ crawal/engine/concurrentEngine2.go
@@ -0,0 +1,58 @@
+package engine
+
+import (
+	"log"
+)
+
+type ConcurrentEngine2 struct {
+	Scheduler   Scheduler2
+	WorkerCount int
+}
+
+type Scheduler2 interface {
+	Submit(request Request)
+	ConfigureMasterWorkerChan(chan Request)
+	WorkerReady(chan Request)
+	Run()
+}
+
+func (e *ConcurrentEngine2) Run(seeds ...Request) {
+	out := make(chan ParseResult)
+	e.Scheduler.Run()
+
+	for i := 0; i < e.WorkerCount; i++ {
+		createWorker2(out, e.Scheduler)
+	}
+
+	for _, r := range seeds {
+		e.Scheduler.Submit(r)
+	}
+
+	count := 0
+	for {
+		result := <-out
+		for _, item := range result.Items {
+			log.Printf("Got item #%d: %v", count, item)
+			count++
+		}
+
+		for _, request := range result.Requests {
+			e.Scheduler.Submit(request)
+		}
+	}
+}
+
+func createWorker2(out chan ParseResult, s Scheduler2) {
+	in := make(chan Request)
+	go func() {
+		for {
+			s.WorkerReady(in)
+			request := <-in
+			parseResult, err := worker(request)
+			if err != nil {
+				continue
+			}
+			out <- parseResult
+		}
+	}()
+}
