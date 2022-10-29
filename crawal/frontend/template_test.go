24d5098e814b70ffc11499e7b2930f51d753b6fc
zmOnly1 <wenzhimin20>
2022-10-29 23:13:18 +0800

    template

---
+++ crawal/frontend/template_test.go
@@ -0,0 +1,40 @@
+package frontend
+
+import (
+	"learngo2/crawal/engine"
+	"learngo2/crawal/frontend/model"
+	common "learngo2/crawal/model"
+	"os"
+	"testing"
+	"text/template"
+)
+
+func TestTempolate(t *testing.T) {
+	template := template.Must(template.ParseFiles("template.html"))
+	page := model.SearchResult{}
+	page.Hits = 123
+	item := engine.Item{
+		Url: "http://album.zhenai.com/u/1736961045",
+		Id:  "1736961045",
+		Payload: common.Profile{
+			Name:       "Mandy",
+			Marriage:   "未婚",
+			Age:        "32岁",
+			Height:     "158cm",
+			Xinzuo:     "魔羯座(12.22-01.19)",
+			WorkPlace:  "工作地:广州天河区",
+			Income:     "月收入:2-5万",
+			Occupation: "生物工程",
+			Education:  "大学本科",
+		},
+	}
+	for i := 0; i < 10; i++ {
+		page.Items = append(page.Items, item)
+	}
+
+	out, err := os.Create("template.test.html")
+	err = template.Execute(out, page)
+	if err != nil {
+		panic(err)
+	}
+}
